66a96384a343a6f100a73f29fab0134b20ea88a2
andy <juliadi@femaledaily.com>
2016-06-09 12:21:35 +0700

    update mockup additional type

--- application/modules/md_home/views/vw_home_3_menu_counter.php
+++ application/modules/md_home/views/vw_home_3_menu_counter.php
@@ -79,11 +79,63 @@
 
 <div style="margin-top:10px;">
 <ul class="dropdown-verticalx">
+	<li><?php echo"<a href='javascript:void(0)' id='additionalType' ><img src='images/request.png' width=20 height=20 >&nbsp;&nbsp;Additional Type</a>"; ?>
 	<li><?php echo"<a href='javascript:void(0)' onclick='specialFeature()' ><img src='images/logout.png' width=20 height=20 >&nbsp;&nbsp;Logout (".$this->session->userdata['sName'].")</a>"; ?>
 </ul>
 </div>
 		
 	</div>
+
+<div id="mdl_type" style="position:fixed;top:0;bottom:0;left:0;right:0;background-color:rgba(0,0,0,0.75);display:none;z-index:999;">
+	<div style="width:600px;height:400px;background:#fff;position: absolute;top: 50%;left: 50%;margin-top: -200px;margin-left: -300px;">
+		<div style="display:block;text-align:center;background:cornflowerblue;position:relative;padding:10px;">
+			<div style="position:absolute;right:0;top:10px;"><a id="cls_mdl" href="#" style="padding:10px;">X</a></div>
+			<span>ADDITIONAL TYPE</span>
+		</div>
+		<div class="form_wrap" style="display:block;padding:10px;text-align:center;">
+			<form>
+				<div style="margin-bottom:5px;">
+					<select style="width: 98%;padding: 5px;">
+						<option>------Pilih Tipe 1-------</option>
+					</select>
+				</div>
+				<div style="margin-bottom:5px;">
+					<select style="width: 98%;padding: 5px;">
+						<option>------Pilih Tipe 2-------</option>
+					</select>
+				</div>
+				<div style="margin-bottom:5px;">
+					<select style="width: 98%;padding: 5px;">
+						<option>------Pilih Tipe 3-------</option>
+					</select>
+				</div>
+				<div style="margin-bottom:5px;">
+					<button style="width: 98%;padding: 5px;">Add</button>
+				</div>
+			</form>
+		</div>
+		<div style="display:block;padding:10px;text-align:center;">
+			<div style="height:190px;width:98%;margin:auto;overflow-y: scroll;overflow-x: hidden;">
+				<table border="1" style="width:100%;" cellspacing="0" cellpadding="15">
+					<thead>
+						<tr>
+							<th style="padding:5px;">NO</th>
+							<th>Nama</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td style="padding:5px;text-align:center;">1</td>
+							<td style="padding:5px;">INQUIRY > Activation > Prepaid Product</td>
+							<td style="padding:5px;text-align:center;"><a href="#" style="color:red;">DELETE</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>	
+	</div>
+</div>
 
 	
 <script>
@@ -105,4 +157,18 @@
 		}
 	});
 	
+</script>
+
+<script type="text/javascript">
+	$(function(){
+		$('#additionalType').click(function(e){
+			e.preventDefault();
+			$('#mdl_type').show();
+		});
+
+		$('#cls_mdl').click(function(e){
+			e.preventDefault();
+			$('#mdl_type').hide();
+		});
+	});
 </script>
