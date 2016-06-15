1c7457040d9aa47ae3cd4a385b45b5fae4041014
andy <juliadi@femaledaily.com>
2016-06-16 05:08:43 +0700

    update page tiket

--- application/modules/md_tiket/views/index.php
+++ application/modules/md_tiket/views/index.php
@@ -5,9 +5,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Tiket</title>
+        <style type="text/css">
+        	body{font-family: sans-serif;}
+        	.msg{position:absolute;top:0;right:0;left:0;padding:10px;text-align:center;background:#ffa500;color:#fff;display:none;}
+        	.content-wrap{padding-top:40px;}
+        </style>
     </head>
     <body>
-    	<div>
+    	<div class="msg">Insert tiket berhasil!</div>
+    	<div class="content-wrap">
     		<?php if(!empty($layanan)): ?>
 	    		<?php foreach($layanan as $value): ?>
 	    		<button id="btnCustomer" name="btnCustomer" data-id="<?php echo $value['id_layanan']; ?>"><?php echo $value['nama_layanan']; ?></button>
@@ -22,14 +28,16 @@
     				var me = $(this);
     				var id_layanan = me.data('id');
     				me.attr('disabled', true);
+    				$('.msg').hide();
     				$.ajax({
 						url : '<?php echo site_url("md_tiket/md_tiket/do_tiket"); ?>',
 						type: 'POST',
 						dataType : 'json',
 						data: {id_layanan: id_layanan},
 						success : function(data){
-							console.log(data);
 							me.attr('disabled', false);
+							$('.msg').show();
+							setTimeout(function(){ $('.msg').hide(); }, 2000);
 						}
 					});
     			});
