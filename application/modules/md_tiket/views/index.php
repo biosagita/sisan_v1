fa615e406e4c38d0c9ed733862331b31c0c7bad3
andy <juliadi@femaledaily.com>
2016-06-16 11:02:24 +0700

    update page tiket

--- application/modules/md_tiket/views/index.php
+++ application/modules/md_tiket/views/index.php
@@ -9,7 +9,7 @@
         	body{font-family: sans-serif;}
         	.msg{position:absolute;top:0;right:0;left:0;padding:10px;text-align:center;background:#808080;color:#fff;display:none;}
         	.content-wrap{padding-top:40px;}
-        	#btnCustomer{background-color:#ffa500;border:none;color:#fff;padding:5px 50px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;box-shadow:0 8px 16px 0 rgba(0,0,0,.2),0 6px 20px 0 rgba(0,0,0,.19);cursor:pointer;}
+        	.btnCustomer{background-color:#ffa500;border:none;color:#fff;padding:5px 50px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;box-shadow:0 8px 16px 0 rgba(0,0,0,.2),0 6px 20px 0 rgba(0,0,0,.19);cursor:pointer;}
         </style>
     </head>
     <body>
@@ -17,14 +17,14 @@
     	<div class="content-wrap">
     		<?php if(!empty($layanan)): ?>
 	    		<?php foreach($layanan as $value): ?>
-	    		<button id="btnCustomer" name="btnCustomer" data-id="<?php echo $value['id_layanan']; ?>"><?php echo $value['nama_layanan']; ?></button>
+	    		<button class="btnCustomer" id="btnCustomer_<?php echo $value['id_layanan']; ?>" name="btnCustomer_<?php echo $value['id_layanan']; ?>" data-id="<?php echo $value['id_layanan']; ?>"><?php echo $value['nama_layanan']; ?></button>
 	    		<?php endforeach; ?>
     		<?php endif; ?>
     	</div>
     	<script type="text/javascript" src="<?php echo base_url();?>assets/easyui/jquery.min.js"></script>
     	<script type="text/javascript">
     		$(function(){
-    			$('#btnCustomer').click(function(e){
+    			$('.btnCustomer').click(function(e){
     				e.preventDefault();
     				var me = $(this);
     				var id_layanan = me.data('id');
